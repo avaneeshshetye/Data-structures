650bde1b136e388cea179cca52d4f8ced340bc3f
Avaneesh Shetye <39583458+avaneeshshetye@users.noreply.github.com>
2022-09-08 23:35:23 +0530

    Add files via upload

---
+++ Arrayinitialization/Stack/test.java
@@ -0,0 +1,20 @@
+package Stack;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack stack = new Stack(5);
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		
+		stack.pop();
+		
+		stack.printStack();
+		
+		
+
+	}
+
+}
